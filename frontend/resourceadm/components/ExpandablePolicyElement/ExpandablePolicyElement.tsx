4b5a419c13b3448787b8336440cabbb25b263ea8
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-05-31 13:41:57 +0200

    Implementing dropdown menu component

--- frontend/resourceadm/components/ExpandablePolicyElement/ExpandablePolicyElement.tsx
+++ frontend/resourceadm/components/ExpandablePolicyElement/ExpandablePolicyElement.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode, useState } from 'react';
 import classes from './ExpandablePolicyElement.module.css';
 import { ChevronDownIcon, ChevronUpIcon } from '@navikt/aksel-icons';
+import { DropdownMenu } from './DropdownMenu';
 
 interface Props {
   title: string;
@@ -18,17 +19,43 @@ interface Props {
  */
 export const ExpandablePolicyElement = ({ title: cardTitle, children, isCard = true }: Props) => {
   const [isOpen, setIsOpen] = useState(true);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleClickMoreButton = () => {
+    setIsDropdownOpen((prev) => !prev);
+    console.log('hei');
+  };
+
+  const handleDelete = () => {
+    // TODO: Handle delete action
+  };
+
+  const handleDuplicate = () => {
+    // TODO: Handle duplicate action
+  };
 
   return (
     <div className={isCard ? classes.cardWrapper : classes.elementWrapper}>
-      <button className={classes.expandButton} onClick={() => setIsOpen((v) => !v)}>
-        <p className={classes.title}>{cardTitle}</p>
-        {isOpen ? (
-          <ChevronUpIcon title='Close the card' fontSize='1.8rem' />
-        ) : (
-          <ChevronDownIcon title='Open the card' fontSize='1.8rem' />
-        )}
-      </button>
+      <div className={classes.topWrapper}>
+        <button
+          className={isCard ? classes.cardExpandButton : classes.elementExpandButton}
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <p className={classes.title}>{cardTitle}</p>
+          {isOpen ? (
+            <ChevronUpIcon title='Close the card' fontSize='1.8rem' />
+          ) : (
+            <ChevronDownIcon title='Open the card' fontSize='1.8rem' />
+          )}
+        </button>
+        <DropdownMenu
+          isOpen={isDropdownOpen}
+          handleClickMoreIcon={handleClickMoreButton}
+          handleCloseMenu={() => setIsDropdownOpen(false)}
+          handleDuplicate={handleDuplicate}
+          handleDelete={handleDelete}
+        />
+      </div>
       {isOpen && (
         <div className={isCard ? classes.cardBottomWrapper : classes.elementBottomWrapper}>
           {children}
