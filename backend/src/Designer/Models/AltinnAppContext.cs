b7712c4c1d3e7b6a5e7568990d9ed6d78e1f552b
Mirko Sekulic <misha.sekulic@gmail.com>
2023-08-28 13:05:05 +0200

    Altinn App context validations expanded

--- backend/src/Designer/Models/AltinnAppContext.cs
+++ backend/src/Designer/Models/AltinnAppContext.cs
@@ -1,18 +1,40 @@
-﻿using Altinn.Studio.Designer.Helpers;
+﻿using System;
+using System.Text.RegularExpressions;
+using Altinn.Studio.Designer.Helpers;
 
 namespace Altinn.Studio.Designer.Models
 {
     public class AltinnAppContext
     {
+        public string Org { get; }
+        public string App { get; }
+        public string Developer { get; }
+
         public AltinnAppContext(string org, string app, string developer)
         {
             Guard.AssertValidAppRepoName(app);
+            ValidateDeveloper(developer);
+            ValidateOrganization(org);
+
             Org = org;
             App = app;
             Developer = developer;
         }
-        public string Org { get; }
-        public string App { get; }
-        public string Developer { get; }
+
+        private static void ValidateDeveloper(string developer)
+        {
+            if (!Regex.IsMatch(developer, "^[a-zA-Z0-9][a-zA-Z0-9-_\\.]*$"))
+            {
+                throw new ArgumentException("Provided developer name is not valid");
+            }
+        }
+
+        private void ValidateOrganization(string org)
+        {
+            if (!Regex.IsMatch(org, "^[a-zA-Z0-9][a-zA-Z0-9-_\\.]*$"))
+            {
+                throw new ArgumentException("Provided organization name is not valid");
+            }
+        }
     }
 }
