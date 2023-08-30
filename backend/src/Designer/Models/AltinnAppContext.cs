97920c9216df433dbfc07c7a0e994380c8964b4f
Mirko Sekulic <misha.sekulic@gmail.com>
2023-08-30 08:29:38 +0200

    Altinn repo class

--- backend/src/Designer/Models/AltinnAppContext.cs
+++ backend/src/Designer/Models/AltinnAppContext.cs
@@ -7,29 +7,16 @@ namespace Altinn.Studio.Designer.Models
     /// <summary>
     /// Context of an app. Holding information about org, app and developer
     /// </summary>
-    public class AltinnAppContext
+    public class AltinnAppContext: AltinnRepo
     {
-        /// <summary>
-        /// The organization owning the repository identfied by it's short name as defined in Gitea.
-        /// </summary>
-        public string Org { get; }
-        /// <summary>
-        /// The name of the repository as specified in Gitea.
-        /// </summary>
-        public string App { get; }
         /// <summary>
         /// The user name of the developer working on the repository.
         /// </summary>
         public string Developer { get; }
 
-        public AltinnAppContext(string org, string app, string developer)
+        public AltinnAppContext(string org, string app, string developer): base(org, app)
         {
-            Guard.AssertValidAppRepoName(app);
             ValidateDeveloper(developer);
-            ValidateOrganization(org);
-
-            Org = org;
-            App = app;
             Developer = developer;
         }
 
@@ -41,12 +28,6 @@ namespace Altinn.Studio.Designer.Models
             }
         }
 
-        private void ValidateOrganization(string org)
-        {
-            if (!Regex.IsMatch(org, "^[a-zA-Z0-9][a-zA-Z0-9-_\\.]*$"))
-            {
-                throw new ArgumentException("Provided organization name is not valid");
-            }
-        }
+
     }
 }
---
+++ backend/src/Designer/Models/AltinnRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Altinn.Studio.Designer.Helpers;
+
+namespace Altinn.Studio.Designer.Models
+{
+    public class AltinnRepo
+    {
+        /// <summary>
+        /// The organization owning the repository identfied by it's short name as defined in Gitea.
+        /// </summary>
+        public string Org { get; }
+        /// <summary>
+        /// The name of the repository as specified in Gitea.
+        /// </summary>
+        public string App { get; }
+
+        public AltinnRepo(string org, string app)
+        {
+            ValidateOrganization(org);
+            Guard.AssertValidAppRepoName(app);
+            Org = org;
+            App = app;
+        }
+
+        private void ValidateOrganization(string org)
+        {
+            if (!Regex.IsMatch(org, "^[a-zA-Z0-9][a-zA-Z0-9-_\\.]*$"))
+            {
+                throw new ArgumentException("Provided organization name is not valid");
+            }
+        }
+    }
+}
